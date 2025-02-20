ee16b6482773eb8662daa4dd45f1022f1303a7e2
MAG <raphvmag@ik.me>
2025-02-20 23:34:07 +0100

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route'],
+  protectedRoutes: ['/mag-duchamp-stuff', '/Backup/perso/evox_build_duchamp/mag-duchamp-stuff'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:mag@magcloud.eu',
