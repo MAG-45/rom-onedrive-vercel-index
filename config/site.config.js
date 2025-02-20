0d7f3a59ded37ee818671d7ec921e8d5e3a9d75a
MAG <raphvmag@ik.me>
2025-02-20 20:23:53 +0100

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -38,7 +38,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Thanks to every people who help me to make rom ! Onelots, jow4h, duchamp dev team for tree, EvolutioX team and others !',
+    'Thanks to every people who help me to make rom ! Onelots, jow4h, duchamp dev team for tree, EvolutionX team and others !',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
